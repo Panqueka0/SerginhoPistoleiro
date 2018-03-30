570e6c82f07ad4515c0a4eba3bd9efc9f98318e6
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 15:32:47 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,15 +10,13 @@ serginho.on('guildMemberAdd', member => {
 });
 
 serginho.on('message', message => {
-    //console.log(`(${message.server.name} / ${message.channel.name}) ${message.author.name}: ${message.content}`);
-    //message.reply('o que carregas neste caixão?');
     if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
         message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
     }
     else if(message.content === 'oSim'){
         message.reply('otimo, cagão.');
-        const user = message.member;
-        user.addRole('Hired Gun');
+        const usuario = message.member;
+        usuario.addRole('Hired Gun');
     }
     else if(message.content === 'oNão'){
         message.reply('um momento amigo. se quiser entrar depois escreva oIn');
