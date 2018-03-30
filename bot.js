fd468734fbd64cd1daa927e5d4f28792d2eca63d
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 15:25:43 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,29 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const serginho = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+serginho.on('ready', () => {
+    console.log('to pronto');
 });
 
-client.on('message', message => {
+serginho.on('guildMemberAdd', member => {
+   member.send("Ei seu bunda mole!");
+});
+
+serginho.on('message', message => {
     //console.log(`(${message.server.name} / ${message.channel.name}) ${message.author.name}: ${message.content}`);
     //message.reply('o que carregas neste caixão?');
+    if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
+        message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
+    }
+    else if(message.content === 'oSim){
+        message.reply('otimo, cagão.');
+        const user = message.member;
+        user.addRole('Hired Gun');
+    }
+    else if(message.content === 'oNão){
+        message.reply('um momento amigo. se quiser entrar depois escreva oIn');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+serginho.login(process.env.BOT_TOKEN);
