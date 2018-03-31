ab1c82da5ef92c34f97f76dc830f92ef2844730b
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 22:33:13 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,29 +11,27 @@ serginho.on('message', message => {
    /*if (message.isMentioned(serginho.user)){
       
    }*/ 
-   else{
-      if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
-         message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
-      }
-      else if(message.content === 'oSim'){
-         if(message.member.roles.has('429157255720730624')|| 
-            message.member.roles.has('429163812512333826')|| 
-            message.member.roles.has('429159637556920320')){
+   if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
+      message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
+   }
+   else if(message.content === 'oSim'){
+      if(message.member.roles.has('429157255720730624')|| 
+         message.member.roles.has('429163812512333826')|| 
+         message.member.roles.has('429159637556920320')){
 
-            message.reply('um momento amigo, o que levas neste casaco? um distintivo?');
-         }
-         else{
-            message.reply('otimo, cagão.');
-            message.member.addRole('429160036846272527');
-            message.member.addRole('429156624645750784');
-         }      
-      }
-      else if(message.content === 'oNão'){
-         message.reply('acabei de me aborrecer. (oSim quando quiser entrar na gangue)');
+         message.reply('um momento amigo, o que levas neste casaco? um distintivo?');
       }
       else{
-         message.reply('um momento amigo, quer entrar na minha gangue? (oSim/oNão)');
-      }
+         message.reply('otimo, cagão.');
+         message.member.addRole('429160036846272527');
+         message.member.addRole('429156624645750784');
+      }      
+   }
+   else if(message.content === 'oNão'){
+      message.reply('acabei de me aborrecer. (oSim quando quiser entrar na gangue)');
+   }
+   else{
+      message.reply('um momento amigo, quer entrar na minha gangue? (oSim/oNão)');
    }
 });
 
