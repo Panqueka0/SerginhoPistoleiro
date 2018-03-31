2d875f31e6fbf51346e713f2029d55a3e115c518
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 22:14:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,11 +9,31 @@ serginho.on('guildMemberAdd', member => {
 serginho.on('message', message => {
    if (message.author.bot)return;
    if (message.isMentioned(serginho.user)){
-       recrutar(message);
+      if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
+         message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
+      }
+      else if(message.content === 'oSim'){
+         if(message.member.roles.has('429157255720730624')|| 
+            message.member.roles.has('429163812512333826')|| 
+            message.member.roles.has('429159637556920320')){
+
+            message.reply('um momento amigo, o que levas neste casaco? um distintivo?');
+         }
+         else{
+            message.reply('otimo, cagão.');
+            message.member.addRole('429160036846272527');
+         }      
+      }
+      else if(message.content === 'oNão'){
+         message.reply('acabei de me aborrecer. (oSim quando quiser entrar na gangue)');
+      }
+      /*else{
+         message.reply('um momento amigo, quer entrar na minha gangue? (oSim/oNão)');
+      }*/
    }  
 });
 
-function recrutar(message){
+/*function recrutar(message){
    var usuario=message.member;
    if (message.content === 'falou comigo?' || message.content === 'falou comigo ?') {
       message.reply('sim gracinha, quer entrar na minha gangue? (oSim/oNão)');
@@ -36,7 +56,8 @@ function recrutar(message){
    /*else{
       message.reply('um momento amigo, quer entrar na minha gangue? (oSim/oNão)');
    }*/
-}
+///
+
 
 // THIS  MUST  BE  THIS  WAY
 serginho.login(process.env.BOT_TOKEN);
