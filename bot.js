1538a92c38c806c302e3dcaff86a1a72a6379657
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-04-03 21:46:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const serginho = new Discord.Client();
 
 serginho.on('guildMemberAdd', member => {
-   //member.guild.channels.get('414151682264662016').send(member.user.toString()+' Ei seu bunda mole');
-   member.guild.channels.get('414151682264662016').send(`${member} Ei seu bunda mole`);
+   //member.guild.channels.get('400751507152175107').send(member.user.toString()+' Ei seu bunda mole');
+   member.guild.channels.get('400751507152175107').send(`${member} Ei seu bunda mole`);
 });
 
 serginho.on('message', message => {
